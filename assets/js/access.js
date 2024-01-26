4170825f5041d09da676fe4db03ee06a6d59d76a
Squash <107418778+WorkSquash@users.noreply.github.com>
2024-01-26 18:01:41 +0100

    Delete assets directory

--- assets/js/access.js
+++
@@ -1,7 +0,0 @@
-function checkInput() {
-    var inField = document.getElementById('inputField').value.toLowerCase();
-    var passCode = 'projects@bbk.hu';
-    if (inField.includes(passCode)) {
-        window.location.href = '../html/project-management.html';
-    }
-}
