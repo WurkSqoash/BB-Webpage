1f551a044766b3ffa4cc8da0f7a6f1f7c51fc263
Squash <marcellszabadosi@gmail.com>
2024-01-28 15:20:20 +0100

    fixed projects.js

--- webpage/assets/js/projects.js
+++ webpage/assets/js/projects.js
@@ -4,6 +4,9 @@ function clearInputField() {
     document.getElementById('inputField').value = '';
 }
 
+var fakeIp = Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256) + "." + Math.floor(Math.random() * 256);
+console.log(fakeIp + '\nEz nem a te ip címed ez egy véletszerűen generált ip cím!!'); //For testing porpuses only this shouldn't generate real ip addresses i hope...
+
 window.addEventListener('pageshow', clearInputField);
 document.getElementById('inputField').addEventListener('input', checkInput);
 
@@ -11,13 +14,14 @@ function checkInput() {
     var inputField = document.getElementById('inputField').value.toLowerCase();
     var validCodes = {
         'bbk-0001': '../assets/pdf/0001.pdf',
-        'bbk-0002': '../assets/pdf/0001.pdf',
+        'bbk-0002': '../assets/pdf/0001.pdf'
     };
 
     if (Object.keys(validCodes).includes(inputField)) {
         window.open(validCodes[inputField], '_blank');
         clearInputField();
-    } else {
+    } 
+    /*if(!Object.keys(validCodes).includes(inputField) && finishedTyping) {
         alert('Helytelen Azonosító!\nProbáld Újra!');
-    }
+    }*/
 }
